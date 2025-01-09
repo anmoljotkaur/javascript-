dcfe8f698119134e71877e57b8e488ef63521160
anmoljotkaur <129553908+anmoljotkaur@users.noreply.github.com>
2025-01-09 15:07:17 -0600

    mathematical function sign intro

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -54,3 +54,13 @@ console.log(resultt);
 let result4 = email.replace('m', 'w');
 console.log(result4);
 
+let radius = 10;
+const pi = 3.14;
+console.log(radius, pi);
+
+//math operators 
+console.log(10/2);
+let answer = radius % 3;
+
+
+
