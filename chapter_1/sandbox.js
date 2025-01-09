004d26e3f5fa68786920db8d2abe277b3a90bd27
anmoljotkaur <129553908+anmoljotkaur@users.noreply.github.com>
2025-01-09 13:59:31 -0600

    # data types and variables

---
+++ chapter_1/sandbox.js
@@ -0,0 +1,37 @@
+let age = 25;
+let year = 2002
+console.log(age, year);
+
+age = 26;
+year = 2003
+console.log(age, year);
+
+const number = 2002;
+console.log(number);
+
+
+//strings
+console.log('hello, world');
+
+let email = 'anmol@123.ca';
+console.log(email);
+
+// string concatenation
+
+let firstname = 'anmol';
+let lastname = 'jot';
+let fullname = firstname + '' + lastname;
+console.log(fullname);
+
+//getting chracters
+console.log(fullname[0]);
+console.log(fullname[2]);
+
+//string length
+console.log(fullname.length);
+
+//string methods
+console.log(fullname.toUpperCase());
+let result = fullname.toUpperCase();
+console.log(result);
+console.log(result, fullname);gigoi
