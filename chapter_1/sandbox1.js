37183054999da17366a5a909ebfaadb288c4deaa
anmoljotkaur <129553908+anmoljotkaur@users.noreply.github.com>
2025-01-10 08:24:05 -0600

    join method

--- chapter_1/sandbox1.js
+++ chapter_1/sandbox1.js
@@ -1,8 +1,13 @@
 let ninjas = ['shaun','ruy', 'chuli' ];
-console.log(ninjas[1]);
+// console.log(ninjas[1]);
 
-ninjas[1] ='ken';
-console.log(ninjas[1]);
+// ninjas[1] ='ken';
+// console.log(ninjas[1]);
 
-let ages = [10, 20, 30, 56];
-console.log(ages[2]);
+// let ages = [10, 20, 30, 56];
+// console.log(ages[2]);
+
+//array methods
+// let result = ninjas.join(',');
+let result = ninjas.indexOf('chuli');
+console.log(result);
