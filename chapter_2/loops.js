9209fb32b4da02a3394a2362ba415009cde3ed73
anmoljotkaur <129553908+anmoljotkaur@users.noreply.github.com>
2025-01-10 09:59:40 -0600

    array in for loops

--- chapter_2/loops.js
+++ chapter_2/loops.js
@@ -1,7 +1,15 @@
 //control flow
 
 //for loops
-for(let i = 0; i < 5; i++){
-console.log('in loop: ',i);
+// for(let i = 0; i < 5; i++){
+// console.log('in loop: ',i);
+// }
+// console.log('loop finished');
+
+const names = ['shaun', 'mario', 'luigui'];
+
+for(let i = 0; i < names.length; i++){
+// console.log(names[i]); // give the name of the diffreent array elements
+  let html = `<div>${names[i]}</div>`
+  console.log(html);   
 }
-console.log('loop finished');
