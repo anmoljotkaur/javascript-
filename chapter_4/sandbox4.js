4f90aa07560e427787138c9a1e64ec1b88ca2372
anmoljotkaur <129553908+anmoljotkaur@users.noreply.github.com>
2025-01-13 14:35:07 -0600

    math object

--- chapter_4/sandbox4.js
+++ chapter_4/sandbox4.js
@@ -2,26 +2,8 @@
 //for example: blog object properties (title, content, author)
 //methods (publish, unpublish, delete)
 
-let user = {
-    name: 'anmol',
-    age: 23,
-    email: 'anmol@123',
-    location: 'winnipeg',
-    blogs: ['why mac and cheese rules', '10 things to make with mermite'],
-    login: function(){
-        console.log('the user logged in');
-    },
-    logout: function(){
-        console.log('the user logged out');
-    }
-
-};
-
-user.login();
-user.logout();
-
-const name = 'mario';
-name.toUpperCase();
+// const name = 'mario';
+// name.toUpperCase();
 // console.log(user);
 // console.log(user.name);
 
@@ -32,3 +14,58 @@ name.toUpperCase();
 // user['name'] = 'ANU';
 // console.log(user.name);
 
+// let user = {
+//     name: 'anmol',
+//     age: 23,
+//     email: 'anmol@123',
+//     location: 'winnipeg',
+//     blogs: ['why mac and cheese rules', '10 things to make with mermite'],
+//     login: function(){
+//         console.log('the user logged in');
+//     },
+//     logout: function(){
+//         console.log('the user logged out');
+//     },
+//     logBlogs: function(){
+        
+//         //console.log(this.blogs);  //user objects
+//         this.blogs.forEach(blog =>{
+//             console.log(blog);
+//         })
+
+//     }
+
+// };
+// user.logBlogs();
+// console.log(this)
+// // user.login();
+// // user.logout();
+
+// // const name = 'mario';
+// // name.toUpperCase();
+// // console.log(user);
+// // console.log(user.name);
+
+// // user.age = 24;
+// // console.log(user.age);
+
+// // console.log(user['name']);
+// // user['name'] = 'ANU';
+// // console.log(user.name);
+
+//math object
+console.log(Math);
+console.log(Math.PI);
+
+const area = 7.7;
+console.log(Math.round(area)); //round of to the nearest integer
+console.log(Math.floor(area)); //round of to the less number 7
+//it does convert 7.1 == 8
+console.log(Math.ceil(area));
+
+console.log(Math.trunc(area));
+
+//random numbers
+const random = Math.random();
+console.log(random)  //generate random numbers
+console.log(Math.round(random*100)); //generate randome number between 1 10 100
