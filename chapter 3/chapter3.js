da51e9ab8b40bdb04235328d681236b33b943867
anmoljotkaur <129553908+anmoljotkaur@users.noreply.github.com>
2025-01-13 09:54:38 -0600

    practice more complex arrow function

--- chapter 3/chapter3.js
+++ chapter 3/chapter3.js
@@ -45,6 +45,35 @@
 // }
 
 //we can write above like that by removing the curley braces because it is really simple.
-const calcArea = radius => 3.14 *radius **2;
-const area = calcArea(5);
-console.log(area);
+// const calcArea = radius => 3.14 *radius **2;
+// const area = calcArea(5);
+// console.log(area);
+
+
+//practice arrow function
+// const greet = () => 'hello, world';
+// const result = greet();
+// console.log(result);
+
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+
+// console.log(bill([10, 15, 30], 0.2));
+
+
+//converting the above into arrow function
+const bill = (products, tax)=>{
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+console.log(bill([10, 15, 30], 0.2));
