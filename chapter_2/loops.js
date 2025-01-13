e687ad0b62c4bb1a3a02770ab8f7cd3f158e919a
anmoljotkaur <129553908+anmoljotkaur@users.noreply.github.com>
2025-01-13 08:59:10 -0600

    variables and block scope

--- chapter_2/loops.js
+++ chapter_2/loops.js
@@ -81,14 +81,49 @@ const names = ['shaun', 'mario', 'luigui'];
 
 //break and continue
 
-const scores =[ 50, 25, 0, 30, 100, 20, 10]
-for(let i = 0; i < scores.length; i++){
-     if(scores[i] === 0){
-     continue;
-    }
-    console.log('your score: ', scores[i]);
-    if(scores[i] === 100){
-        console.log('congrates, you got the top scores');
-        break;
-    }
+// const scores =[ 50, 25, 0, 30, 100, 20, 10]
+// for(let i = 0; i < scores.length; i++){
+//      if(scores[i] === 0){
+//      continue;
+//     }
+//     console.log('your score: ', scores[i]);
+//     if(scores[i] === 100){
+//         console.log('congrates, you got the top scores');
+//         break;
+//     }
+// }
+
+//switch statements
+// const grade = 'P'
+// switch (grade){
+//     case 'A':
+//         console.log('you got an A!  ');
+//         break;
+//     case 'B':
+//         console.log('you got an B!  ');
+//         break;
+//     case 'C':
+//         console.log('you got an C!  ');
+//         break;
+//     case 'D':
+//         console.log('you got an D!  ');
+//         break;
+//     default :
+//         console.log('not a valid grade');
+
+
+//VARIBALES & block scope
+let age = 30;
+if(true){
+   let age = 40;
+   let name = 'shaun';
+   console.log('inside 1st code block'  , age, name );
+
+   if(true){
+    console.log('inside side the code block:', age);
+   }
 }
+console.log('outside the code block :' , age);
+
+
+
